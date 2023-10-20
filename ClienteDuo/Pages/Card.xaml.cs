94642666f7f79c3605b9e8de35aa6e6a2664672e
Luigi573 <xavier.arian@gmail.com>
2023-10-19 18:47:43 -0600

    Game table changes
    Added main game window, the party settings toggleable menu is not finished, and cards can't be played

---
+++ ClienteDuo/Pages/Card.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ClienteDuo.Pages
+{
+    public partial class Card: UserControl
+    {
+        string _number; 
+        string _color;
+
+        public Card()
+        {
+            InitializeComponent();
+            _number = "2";
+            _color = "000000";
+        }
+
+        public string Color
+        {
+            set 
+            { 
+                _color = value; 
+                _colorRectangle.Fill = (SolidColorBrush)(new BrushConverter().ConvertFrom(_color)); 
+            }
+            get => _color;
+        }
+        public string Number
+        {
+            set
+            {
+                _number = value;
+                _numberLabel.Content = _number.ToString();
+            }
+            get => _number;
+        }
+    }
+}
