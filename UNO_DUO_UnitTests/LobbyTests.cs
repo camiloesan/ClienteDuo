28fd2dc941221a3d996d985bd862fe641e4937b7
camiloesan <camiloesan@gmail.com>
2023-11-07 15:12:15 -0600

    tests

---
+++ UNO_DUO_UnitTests/LobbyTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClienteDuo.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClienteDuo.Pages.Tests
+{
+    [TestClass()]
+    public class LobbyTests
+    {
+        int openLobby;
+        [TestMethod()]
+        public void CreateNewPartySuccessTest()
+        {
+            Lobby lobby = new Lobby();
+            openLobby = lobby.CreateNewParty("host");
+            Assert.IsNotNull(openLobby);
+        }
+
+        [TestMethod()]
+        public void ClosePartySuccessTest()
+        {
+            Lobby Lobby = new Lobby();
+            Lobby.CloseParty(openLobby);
+        }
+    }
+}
